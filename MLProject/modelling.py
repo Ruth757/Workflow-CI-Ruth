34fddd39521b8684bcbb6ad0136633a718be0c86
Ruth Anastasya Harefa <ruth.anastasya.h@mail.ukrim.ac.id>
2026-06-07 21:44:02 +0700

    Fix MLflow Project workflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,45 +3,36 @@ import mlflow
 import mlflow.sklearn
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, classification_report
 
 # Aktifkan autolog MLflow
-mlflow.set_experiment("Telco_Churn")
 mlflow.sklearn.autolog()
 
 # Load dataset
 X_train = pd.read_csv("X_train.csv")
 X_test = pd.read_csv("X_test.csv")
 
-y_train = pd.read_csv("y_train.csv")
-y_test = pd.read_csv("y_test.csv")
+y_train = pd.read_csv("y_train.csv").values.ravel()
+y_test = pd.read_csv("y_test.csv").values.ravel()
 
-# Ubah target menjadi array
-y_train = y_train.values.ravel()
-y_test = y_test.values.ravel()
+# Model
+model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
 
-# Mulai MLflow Run
-with mlflow.start_run():
+# Training
+model.fit(X_train, y_train)
 
-    # Model
-    model = RandomForestClassifier(
-        n_estimators=100,
-        random_state=42
-    )
+# Prediksi
+y_pred = model.predict(X_test)
 
-    # Training
-    model.fit(X_train, y_train)
+# Evaluasi
+accuracy = accuracy_score(y_test, y_pred)
 
-    # Prediksi
-    y_pred = model.predict(X_test)
+print(f"Accuracy : {accuracy:.4f}")
 
-    # Evaluasi
-    accuracy = accuracy_score(y_test, y_pred)
-
-    print(f"Accuracy : {accuracy:.4f}")
-
-    print("\nClassification Report")
-    print(classification_report(y_test, y_pred))
+print("\nClassification Report")
+print(classification_report(y_test, y_pred))
 
 print("Training selesai dan tercatat di MLflow")
