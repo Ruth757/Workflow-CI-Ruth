7bfdf20b1096c1706b5be5ce62c3e100fd8eea8c
Ruth Anastasya Harefa <ruth.anastasya.h@mail.ukrim.ac.id>
2026-06-09 23:03:53 +0700

    Fix MLflow CI pipeline and modelling path

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import mlflow
 import mlflow.sklearn
@@ -5,18 +6,21 @@ import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
-# MLflow autolog
 mlflow.sklearn.autolog()
 
-# Load data (HARUS DI DALAM MLProject/data/)
-X_train = pd.read_csv("data/X_train.csv")
-X_test = pd.read_csv("data/X_test.csv")
-y_train = pd.read_csv("data/y_train.csv").values.ravel()
-y_test = pd.read_csv("data/y_test.csv").values.ravel()
+BASE_DIR = os.path.dirname(__file__)
+
+X_train = pd.read_csv(os.path.join(BASE_DIR, "dataset_preprocessing/X_train.csv"))
+X_test  = pd.read_csv(os.path.join(BASE_DIR, "dataset_preprocessing/X_test.csv"))
+y_train = pd.read_csv(os.path.join(BASE_DIR, "dataset_preprocessing/y_train.csv")).values.ravel()
+y_test  = pd.read_csv(os.path.join(BASE_DIR, "dataset_preprocessing/y_test.csv")).values.ravel()
 
 with mlflow.start_run():
 
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
 
     model.fit(X_train, y_train)
 
@@ -24,7 +28,9 @@ with mlflow.start_run():
 
     accuracy = accuracy_score(y_test, y_pred)
 
-    print("Accuracy:", accuracy)
+    print(f"Accuracy: {accuracy:.4f}")
     print(classification_report(y_test, y_pred))
 
     mlflow.log_metric("accuracy", accuracy)
+
+print("Training selesai")
